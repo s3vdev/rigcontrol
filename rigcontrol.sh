15559e279a14c8a4d490fbcf769dad78ca5f5f5c
Sven Mielke <web@ddl.bz>
2018-03-19 18:27:50 +0100

    Bugfixes...

--- rigcontrol.sh
+++ rigcontrol.sh
@@ -76,7 +76,7 @@ YellowEcho(){ echo -e "$(tput setaf 3)$1$(tput sgr0)"; }
 . /home/ethos/rigcheck_config.sh
 
 # Check if vars on rigcheck_config.sh was set
-if [[ -z "${MIN_TOTAL_HASH}" && -z "${LOW_WATT}" && -z "${TOKEN}" && -z "${CHAT_ID}" ]]
+if [[ -z "${MIN_TOTAL_HASH}" && -z "${LOW_WATT}" ]]
 then
     RedEcho "Please setup your vars in /home/ethos/rigcheck_config.sh!";
     exit 1
